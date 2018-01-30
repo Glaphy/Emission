c6a64c77f6382789625ed4ec2fcb1b38f1b9380e
Siddhartha Menon <>
2018-01-30 17:47:16 +0000

    Extremely rudimentary code. Solves closed boundary probles on very simple geometries

--- fpstar5x5.m
+++ fpstar5x5.m
@@ -1,33 +1,11 @@
-inputmatrix = [100 100 100 100 100;
-                0   8   8   8   0;
-                0   8   8   8 	0;
-                0   8   8   8   0;
-                0   0   0   0   0]; %NxN matrix
+b = [100 100 100 100 100 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]' %NxN matrix
             
-N = length(inputmatrix);
-n = N^2 - 4
+L = length(inputmatrix)+1;
+F=[-4 1 0 0 0; 1 -4 1 0 0; 0 1 -4 1 0; 0 0 1 -4 1; 0 0 0 1 -4];
+I=[1 0 0 0 0; 0 1 0 0 0; 0 0 1 0 0; 0 0 0 1 0; 0 0 0 0 1];
+N=[0 0 0 0 0; 0 0 0 0 0; 0 0 0 0 0; 0 0 0 0 0; 0 0 0 0 0];
+A=[F I N N N; I F I N N; N I F I N; N N I F I; N N N I F];
 
-%8 stands for unknown
-solvematrix = zeros(n,n);
-solvevector = zeros(n,1); %A\b to solve Ax = b for x
-c = 1;
+V=A\b;
 
-for i = [1:N]
-    for j = [1:N]
-        if (i==1 && j==1) || (i==1 && j==N) || (i==N && j==1) || (i==N && j==N)
-        c = c - 1;
-            
-        elseif inputmatrix(i,j) ~= 8 %i.e. if it is known
-            solvematrix(c,c) = 1;
-            solvevector(c,1) = inputmatrix(i,j);
-        elseif c <= round(n/2)
-            solvematrix(c,c-3:c+2) = [1 0 0 4 0 1];
-        else
-            solvematrix(c,c-2:c+3) = [1 0 4 0 0 1];
-        end
-        c = c + 1;
-    end
-end
 
-solution = solvematrix\solvevector;
-solution'
