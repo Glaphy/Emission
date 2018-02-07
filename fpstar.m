1d6214d1fc48eab8c9d4288f055e84d9b238357a
Siddhartha Menon <>
2018-02-07 14:47:41 +0000

    Started implementing possible boundary condition fix.

--- fpstar.m
+++ fpstar.m
@@ -1,10 +1,14 @@
 %NxN matrix
-N=151;
+N=81;
 
 b=zeros(N^2,1);
-b(round(N^2/2),1)=-100;
+b(1:N,1)=100;
+%b(round(N^2/2),1)=-100;
 
 A=tridiag(N);
+A(1:N,:)=0;
+A(1:N,1:N)=diag(ones(N,1));
+
 
 V=A\b;
 S=vecreform(V,N);
