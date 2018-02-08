77fdda472eb4012679fb53d371c6bc5924999ef7
Siddhartha Menon <>
2018-02-08 18:33:54 +0000

    Implemented possible fix for extra unknown rows in the png array.

--- imgexperiments/readpng.c
+++ imgexperiments/readpng.c
@@ -3,11 +3,11 @@
 #include <string.h>
 #include <math.h>
 
-#define STB_IMAGE_IMPLEMENTATION
 #define STBI_ONLY_PNG
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-#define CHANNEL_NO 3
+#define CHANNEL_NUM 3
 
 int main(int argc, char *argv[]) {
 	int width, height, bpp;
@@ -27,11 +27,10 @@ int main(int argc, char *argv[]) {
 		exit(2);
 	}
 
-	unsigned char *rgb_image=stbi_load(filename, &width, &height, &bpp, CHANNEL_NO);
-	printf("%dx%d\n%d\n", height, width, bpp);
+	unsigned char *rgb_image=stbi_load(filename, &width, &height, &bpp, CHANNEL_NUM);
     
-	for(int i=0; i<height*CHANNEL_NO; i++){
-		for(int j=0; j<width*CHANNEL_NO; j++){
+	for(int i=0; i<height; i++){
+		for(int j=0; j<width*CHANNEL_NUM; j++){
 			printf("%3d ", *(rgb_image+i*width+j));
 		}
 
