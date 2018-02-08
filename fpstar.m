87a6c067adf95699a2aa0257085ee6d4d615a1ea
Siddhartha Menon <>
2018-02-08 23:38:40 +0000

    Created the sister tool to vecreform. Takes in a matrix and turns it into a vector.

--- fpstar.m
+++ fpstar.m
@@ -1,13 +1,16 @@
 %NxN matrix
-N=81;
+N=15;
 
 b=zeros(N^2,1);
 b(1:N,1)=100;
+b(round(N^2/2),1)=100
 %b(round(N^2/2),1)=-100;
 
 A=tridiag(N);
 A(1:N,:)=0;
+A(round(N^2/2),:)=0;
 A(1:N,1:N)=diag(ones(N,1));
+A(round(N^2/2),round(N^2/2))=1;
 
 
 V=A\b;
