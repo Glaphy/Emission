34f290f727f763c3ef1c358ae12bbbe327a9b814
Antonio Smecca <antoniosmecca@glaroam2-132-58.wireless.gla.ac.uk>
2018-02-18 19:52:28 +0000

    added comments

--- EasyPlotting.cpp
+++ EasyPlotting.cpp
@@ -8,13 +8,23 @@ using namespace std;
 int main(){
 
   ofstream inFile;
-  inFile.open("PlottingFile");
+  inFile.open("PlottingFile"); //Opening plotting file
+
+  //Setting Stuff
   inFile << "set pm3d\n";
   inFile << "set palette rgb 22,13,-31\n";
   inFile << "set terminal postscript color\n";
+
+  //Setting Output (this can be changed)
   inFile << "set output \"GnuPlates.eps\" \n";
+
+  //Plotting datafile (be sure the datafile is present in the directory)
   inFile << "splot \"Plates.dat\" with lines\n";
+
+  //Closing file
   inFile.close();
+
+  //plotting with gnuplot
   system("gnuplot -p <PlottingFile");
 
   return 0;
--- EasyQiver.cpp
+++ EasyQiver.cpp
@@ -7,13 +7,22 @@ using namespace std;
 int main (){
 
   ofstream inFile;
-  inFile.open("QuiverFile");
+  inFile.open("QuiverFile");//Opening Plotting File
+
+  //Setting stuff
   inFile << "reset\n";
-  //inFile << "set palette rgb 22,13,-31\n";
   inFile << "set terminal postscript color" << endl;
+
+  //Setting Output (this can be changed)
   inFile << "set output \"QuivGrad.eps\" \n";
+
+  //Plotting Datafile (be sure the datafile is present in the directory)
   inFile << "plot \"grad.dat\" using 1:2:3:4 with vectors nofilled head lw 1\n";
+
+  //Closing file
   inFile.close();
+
+  //plotting with gnuplot
   system("gnuplot -p <QuiverFile");
 
   return 0;
